fc6dd4c2b0e079201f14834bb2d23f10dfd1def0
pedrombmachado <pedro.baptistamachado@ntu.ac.uk>
2019-03-04 15:55:40 +0000

    updates

--- push.sh
+++ push.sh
@@ -1,4 +1,8 @@
 #! /bin/bash
+echo "# Directory Structure" > directorySctructure.md
+echo "```" >> directorySctructure.md
+tree ../VCS-1 >> directorySctructure.md
+echo "```" >> directorySctructure.md 
 git add *
 git commit -m "updates"
 git push
