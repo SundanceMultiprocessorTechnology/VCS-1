82fce7f04879cdd64d4ed48f8fd73e93f84e727c
Pedro Machado <pedro.baptistamachado@ntu.ac.uk>
2019-05-04 16:27:34 +0100

    Update install_realsense2_VCS-1.sh

--- Software/Image_processing/VCS-1/install_realsense2_VCS-1.sh
+++ Software/Image_processing/VCS-1/install_realsense2_VCS-1.sh
@@ -4,7 +4,7 @@ echo "Updating the OS..."
 sudo apt update 
 sudo apt upgrade -y
 echo "Installing dependencies..."
-sudo apt install git libssl-dev libusb-1.0-0-dev pkg-config libgtk-3-dev -y
+sudo apt install git libssl-dev libusb-1.0-0-dev pkg-config libgtk-3-dev gcc-6 g++-6 -y
 
 echo "Fetching and unpacking librealsense2 ..."
 mkdir -p $HOME/repositories
