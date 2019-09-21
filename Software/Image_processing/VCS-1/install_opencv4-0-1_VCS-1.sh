9b904e3bd97e2b3b9d35ef23fbfb91984bd1c3a3
Pedro Machado <pedro.baptistamachado@ntu.ac.uk>
2019-09-21 20:58:25 +0100

    Update install_opencv4-0-1_VCS-1.sh

--- Software/Image_processing/VCS-1/install_opencv4-0-1_VCS-1.sh
+++ Software/Image_processing/VCS-1/install_opencv4-0-1_VCS-1.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
-echo "Script for installing the OpenCV 4.0.1 on Ubuntu 18.04 LTS (VCS-1)"
+VERSION="4.1.1"
+echo "Script for installing the OpenCV $VERSION on Ubuntu 18.04 LTS (VCS-1)"
 echo "Updating the OS..."
 sudo apt update 
 sudo apt upgrade -y
@@ -13,11 +14,11 @@ sudo apt install libatlas-base-dev gfortran -y
 sudo apt install python3-dev -y
 sudo apt install libtbb-dev -y
 
-echo "Fetching and unpacking OpenCV 4.0.1..."
+echo "Fetching and unpacking OpenCV $VERSION..."
 mkdir -p $HOME/repositories
 cd $HOME/repositories
-wget -O opencv.zip https://github.com/opencv/opencv/archive/4.0.1.zip
-wget -O opencv_contrib.zip https://github.com/opencv/opencv_contrib/archive/4.0.1.zip
+wget -O opencv.zip https://github.com/opencv/opencv/archive/$VERSION.zip
+wget -O opencv_contrib.zip https://github.com/opencv/opencv_contrib/archive/$VERSION.zip
 wget https://bootstrap.pypa.io/get-pip.py
 sudo python3 get-pip.py
 sudo -H pip3 install numpy matplotlib imutils -U
@@ -25,11 +26,11 @@ unzip opencv.zip
 unzip opencv_contrib.zip
 rm opencv.zip
 rm opencv_contrib.zip
-cd opencv-4.0.1
+cd opencv-$VERSION
 mkdir -p build
 cd build
 
-echo "Compiling OpenCV 4.0.1 ... this will take several minutes..."
+echo "Compiling OpenCV $VERSION ... this will take several minutes..."
 cmake -D CMAKE_BUILD_TYPE=RELEASE \
 	-D CMAKE_INSTALL_PREFIX=/usr/local \
 	-D INSTALL_PYTHON_EXAMPLES=OFF \
@@ -41,10 +42,10 @@ cmake -D CMAKE_BUILD_TYPE=RELEASE \
 	-D WITH_OPENMP=ON \
 	-D PYTHON_EXECUTABLE=/usr/bin/python3 \
 	-D WITH_TBB=ON BUILD_TBB=ON \
-	-D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib-4.0.1/modules ..
+	-D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib-$(VERSION)/modules ..
 echo "Compilation has started ..."
-make -j2
-echo "Installing OpenCV 4.0.1 ..."
+make -j4
+echo "Installing OpenCV $VERSION ..."
 sudo make install
 sudo ldconfig
 source $HOME/.bashrc
