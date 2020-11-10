43673e23978984da1cb5c05c0b14419923eb256c
Ivica Matić <ivica-matic@outlook.com>
2020-11-10 14:22:42 +0000

    Added code to create a swap file before compiling
    Similar to when installing OpenCV, make complains that it has exhausted host memory. 
    With this edit it does no longer happen and it compiles and install sucessfully.

--- Software/Image_processing/VCS-1/install_realsense2_VCS-1.sh
+++ Software/Image_processing/VCS-1/install_realsense2_VCS-1.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+##! /bin/bash
 echo "Script for installing the librealsense2 on Ubuntu 18.04 LTS (VCS-1)"
 echo "Updating the OS..."
 sudo apt update 
@@ -38,6 +38,10 @@ fi
 # sudo apt install gcc-arm-linux-gnueabihf g++-arm-linux-gnueabihf -y
 # export CC=/usr/bin/arm-linux-gnueabihf-gcc-7
 # export CXX=/usr/bin/arm-linux-gnueabihf-g++-7
+sudo dd if=/dev/zero of=/swapfile bs=64M count=16
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
 cmake -D CMAKE_BUILD_TYPE="Release"\
       -D FORCE_LIBUVC=ON \
       -D BUILD_PYTHON_BINDINGS=true \
@@ -46,9 +50,10 @@ cmake -D CMAKE_BUILD_TYPE="Release"\
 echo "WARNING: Please edit CMakeCache.txt and replace O3 flags by O2 when using gcc-7 and g++-7!"
 
 echo "Compiling librealsense 2... this will take several minutes..."
-make -j2
+make -j4
 sudo make install
+sudo swapoff /swapfile
+sudo rm /swapfile
 echo 'export PYTHONPATH="$PYTHONPATH:/usr/local/lib"' >> ~/.bashrc
 sudo ldconfig
 echo "Done!"
-
